f8be381b6524e0f1a1168e9fe7afd3cc89e8e910
GungDeRaka <aswinraka4@gmail.com>
2022-12-06 14:38:53 +0800

    .

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:wisata_bandung/screens/main_screen.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
